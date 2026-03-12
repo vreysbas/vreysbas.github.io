86a789ae60bb5e6b7de7dcff9c0544b9c9ce370f
Bas Vreys <vreysbas@gmail.com>
2026-03-12 12:31:45 +0100

    Configure deployed tracking proxy URL and wrangler setup

--- order-tracking-config.js
+++ order-tracking-config.js
@@ -1,5 +1,5 @@
 window.ORDER_TRACKING_CONFIG = {
   enabled: true,
-  proxyUrl: "",
-  proxyToken: ""
+  proxyUrl: "https://congaxd-order-tracking.conga.workers.dev",
+  proxyToken: "4f1bd4da51a04bd690652cca1542903aa2cced3e36564013"
 };
