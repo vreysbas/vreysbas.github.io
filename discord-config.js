89f29c74046ea30b020e45f044b200482e850665
Bas Vreys <vreysbas@gmail.com>
2026-03-12 11:13:30 +0100

    Add explicit channel picker for Discord admin messages

--- discord-config.js
+++ discord-config.js
@@ -5,6 +5,7 @@ window.DISCORD_CONFIG = {
   orderNotificationChannelId: "1481592352303874138",
   orderMentionUserId: "702458001990090801",
   broadcastChannels: {
-    "1481592352303874138": "https://discord.com/api/webhooks/1481592856958337095/WU2dR6qWYq-C4YJ2BOzJD6IPV4Sy-Nd6FvBFiH1Dyn9_Ydhrfjt3w1zBOh4numPBwmsI"
+    "1481592352303874138": "https://discord.com/api/webhooks/1481592856958337095/WU2dR6qWYq-C4YJ2BOzJD6IPV4Sy-Nd6FvBFiH1Dyn9_Ydhrfjt3w1zBOh4numPBwmsI",
+    "1481592280686268560": "https://discord.com/api/webhooks/1481595669818114179/NDcK1mu3YanpnqsXhut1vOPKjE1vAqiRrwFgCgRD8pOF-DlMyGZ36tRfP5CSX0A10BDv"
   }
 };
