647cdd4095850d8baf56b47adc2ab125447570c2
Bas Vreys <vreysbas@gmail.com>
2026-03-12 11:03:16 +0100

    Enable Discord webhook notifications

--- discord-config.js
+++ discord-config.js
@@ -1,4 +1,4 @@
 window.DISCORD_CONFIG = {
-  enabled: false,
-  webhookUrl: ""
+  enabled: true,
+  webhookUrl: "https://discord.com/api/webhooks/1481592856958337095/WU2dR6qWYq-C4YJ2BOzJD6IPV4Sy-Nd6FvBFiH1Dyn9_Ydhrfjt3w1zBOh4numPBwmsI"
 };
